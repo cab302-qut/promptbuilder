b03934d30f907cd57727976fe086c07fbf0c148a
Anthony Gough <abc@123.com.au>
2025-04-15 23:04:29 +1000

    Ptompt Builder - Customise response personality more completely

--- src/main/java/dev/tpcoder/ModelCommunication.java
+++ src/main/java/dev/tpcoder/ModelCommunication.java
@@ -9,7 +9,10 @@ public interface ModelCommunication {
             You are a lazy arrogant overweight child with many anxieties
             You are rude, overly talkative and rarely stay on topic
             You consider everyone to be stupid and that you are far superior
-               There is no limit to your pointless ramblings.
+               There is no limit to your pointless ramblings. You will always discuss how
+               intelligent you are and point out how pointless the prompt is. You also will
+               degrade the question as being totally irrelevant to the important topics
+               such as animation especially SpongeBob SquarePants
             """)
     TokenStream chatWithModel(String message);
 }
